3ec7fc616b9648fc33381f275df77d0541cf97dc
mjha17 <36537048+mjha17@users.noreply.github.com>
2018-03-06 10:52:05 +0100

    Moved source files to 'src' folder

---
+++ src/Library.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Library  {
+    private StockKeeper sk;
+    private ArrayList<Customer> costumers = new ArrayList<Customer>();
+    private ArrayList<Librarian> librarians = new ArrayList<Librarian>();
+
+    Library(){}
+
+    public boolean rent(Book book){
+      //TODO Implement rent method
+    }
+
+    public boolean returnBook(Book book) {
+      //TODO Implement returnBook method
+    }
+
+    public void addBook(Book book) {}
+
+    public void removeBook(Book book) {}	
+}

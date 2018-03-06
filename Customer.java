7fc953fad12f900db51a1889a478e8db1a65e1d5
Christian Beltoft Ørbæk <36537061+CyborgDragon@users.noreply.github.com>
2018-03-06 10:36:59 +0100

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,4 +1,19 @@
 
-public abstract class Customer {
+public class Customer extends Person {
 
+	private Book[] = new book[5]
+	private int id
+	public boolean rent(Book book) {
+		
+	}
+	public boolean returnBook(Book book) {
+		
+	}
+	private addBook(Book book) {
+		
+	}
+	private removeBook(Book book) {
+		
+	}
+	
 }
